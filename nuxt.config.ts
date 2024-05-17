559611fcb8acc124e253bf3bd0c4895ff933c3eb
Masked <trist13579@gmail.com>
2024-05-16 22:02:05 -0400

    Animated Landing Page Text

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,15 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
+  tailwindcss: {
+    cssPath: ['~/assets/css/tailwind.css', { injectPosition: "first" }],
+    configPath: 'tailwind.config',
+    exposeConfig: {
+      level: 2
+    },
+    config: {},
+    viewer: true,
+  },
   supabase: {
     redirectOptions: {
       login: '/login',
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="flex min-h-96 items-center justify-center text-8xl font-bold bg-gradient-to-r bg-clip-text text-transparent from-indigo-500 via-purple-500 to-indigo-500 animate-text">
+  <div class="flex justify-center items-center min-h-96 m-[5%]">
+  <div
+      class="gradient-text text-8xl font-bold"
+      :style="{ animationDuration: animationDuration }"
+    >
     Rocket Soccer Confederation
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-
+const animationDuration = `5s`
 </script>
 
-<style>
+<style scoped>
+.gradient-text {
+  display: inline-block;
+  background: linear-gradient(90deg, #a963f0, #7a61f3, #7a61f3, #a963f0);
+  background-size: 200% 100%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  animation: scroll-gradient linear infinite;
+}
+
+@keyframes scroll-gradient {
+  0% {
+    background-position: 200% 0;
+  }
+  100% {
+    background-position: -200% 0;
+  }
+}
 </style>
